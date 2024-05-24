49d05c599cb1e36f49bb719336c9c8abf13eee15
Vladimir Patrushev <mkultr@mail.ru>
2024-05-24 10:48:55 +0500

    Закончил сборку справочника

--- Phonebook.py
+++ Phonebook.py
@@ -1,20 +1,20 @@
 def input_name():
-    return ("Введите имя: \n").title()
+    return input("Введите имя: \n").title()
 
 def input_surname():
-    return ("ВВедите фамилию: \n").title()
+    return input("ВВедите фамилию: \n").title()
 
 def input_patronumic():
-    return ("Введите отчество: \n").title()
+    return input("Введите отчество: \n").title()
 
 def input_phone_number():
-    return("Введите номер: \n").title()
+    return input("Введите номер: \n")
 
 def input_address():
-    return ("Введите адресс: \n").title()
+    return input("Введите адрес: \n").title()
 
 def copy_contact():
-    return("Введите номер строки для копирования:\n")
+    return input("Введите номер строки для копирования:\n")
 
 def create_contact():
     surname = input_surname()
@@ -22,28 +22,53 @@ def create_contact():
     patronumic = input_patronumic()
     phone_number = input_phone_number()
     address = input_address()
-    return f'{surname}{name}{patronumic}{phone_number}\n{address}\n\n'
+    return f'{surname} {name} {patronumic} {phone_number}\n{address}\n\n'
 
 def add_contact():
     contact = create_contact()
-    with open('phonebook.txt' 'a', encoding="UTF-8") as file_a:
+    with open('phonebook.txt', 'a', encoding="UTF-8") as file_a:
         file_a.write(contact)
         
-
 def print_contacts():
-    pass
-
-def find_contact():
-    pass
-
+    with open('phonebook.txt', 'r', encoding="UTF-8") as file_r:
+        lst_contacts=file_r.read().rstrip().split("\n\n")
+    for i, contact in enumerate(lst_contacts,1):
+           print(i,contact)   
+        
+def search_contact():
+    print("Варианты поиска:\n\n"
+            "1 - По фамилии контакта\n"
+            "2 - По имени контакта\n"
+            "3 - По отчеству контакта\n"
+            "4 - По адресу контакта\n"
+            "5 - Выход\n"
+            )
+    var = input("Выберите вариант поиска: ")
+    while var not in ('1','2','3','4','5'):
+            print()
+            print("Введены не корректны данные. Введите число от 1 до 5")
+            var = input("Введите вариант поиска: ") 
+    index_var = int(var) - 1    
+    search = input("Введите данные для поиска: ").title()
+    
+    with open('phonebook.txt', 'r', encoding="UTF-8") as file_r:
+        lst_contacts=file_r.read().rstrip().split("\n\n")
+         
+    for contact_str in lst_contacts:
+        contact_lst = contact_str.split()
+        if search in contact_lst[index_var]:
+            print("****************")
+            print(contact_str) 
+            print("****************")
+    
 def copy():
     pass
 
 def interface():
     with open('phonebook.txt', 'a', encoding="UTF-8"):
-            pass
-    command =" "
-    while command !="5":
+        pass    
+    command = ' '
+    while command != '5':
         print("Варианты действий:\n\n"
             "1 - Ввод контакта\n"
             "2 - Вывод контакта\n"
@@ -51,23 +76,23 @@ def interface():
             "4 - Копировать контакт\n"
             "5 - Выход\n"
             )
-        command =input("Введите варант действия:")
+        command = input("Введите вариант действия:")
 
-        while command not in ("1","2","3","4","5"):
+        while command not in ('1','2','3','4','5'):
             print("Введены не корректны данные. Введите число от 1 до 5")
-            command =input("Введите варант действия:")
+            command = input("Введите вариант действия:")
             
         match command:
             
-            case "1":
+            case '1':
                 add_contact()
-            case "2":
+            case '2':
                 print_contacts()
-            case "3":
-                find_contact()
-            case "4":
+            case '3':
+                search_contact()
+            case '4':
                 copy_contact()
-            case "5":
+            case '5':
                 print("Всего доброго!")         
     
 interface()
