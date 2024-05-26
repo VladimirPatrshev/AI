17291b90d83380a6bdbb7b67ae2bf0b178f2d52d
Vladimir Patrushev <mkultr@mail.ru>
2024-05-26 18:56:27 +0500

    Готово задание

--- Attestation.py
+++ Attestation.py
@@ -5,3 +5,10 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+df = pd.DataFrame(data)
+categories = df['whoAmI'].unique()
+one_hot_df = pd.DataFrame()
+for category in categories:
+    one_hot_df[category] = (df['whoAmI'] == category).astype(int)
+    
+print(one_hot_df) 
